de5944c9e447503933619d37ea1662a448c58a18
Diegobrix <dishi.ata@gmail.com>
2023-02-14 00:33:46 -0300

    Estilização Mobile Finalizada

---
+++ js/notifications-handler.js
@@ -0,0 +1,32 @@
+//Elements
+const btnClear = document.querySelector(".notifications-clear");
+const unreadNotifications = document.querySelectorAll(".notification-fancy");
+const display = document.querySelector(".notifications-display");
+
+//Listeners
+btnClear.addEventListener("click", ClearNotifications);
+window.addEventListener("load", NotificationsCounter);
+
+//Functions
+function ClearNotifications() 
+{
+    unreadNotifications.forEach((element) => {
+        element.setAttribute("visualized", "true");
+    });
+
+    display.innerHTML = ("0");
+}
+
+function NotificationsCounter() 
+{
+    let notificationsTotal = 0;
+
+    unreadNotifications.forEach(((item) => {
+        if(item.getAttribute("visualized") == "false") 
+        {
+            notificationsTotal += 1;
+        }
+    }));
+
+    display.innerHTML = (notificationsTotal);
+}
